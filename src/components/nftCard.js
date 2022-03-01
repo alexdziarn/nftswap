1ac71c7c58b9276913a6f0794bfb7b0842a97ffd
alex-windows-pc <alexdziarn@gmail.com>
2022-03-01 15:33:37 -0500

    shows nfts cards

---
+++ src/components/nftCard.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
+import coolmansABI from './abis/coolmansUniverseABI.json';
+import fishyfamABI from './abis/fishyfamABI.json';
+
+const NftCards = (props) => {
+
+  const [nfts, setNfts] = useState([]);
+
+
+
+  async function loadCoolmans() {
+    // loadscoolmans
+    const coolmansContractAddress = "0xa5C0Bd78D1667c13BFB403E2a3336871396713c5";
+    const coolmansContract = new window.web3.eth.Contract(coolmansABI.abi, coolmansContractAddress);
+    coolmansContract.methods.balanceOf(props.userAddress).call().then((bal) => {
+      for(let i = 0; i < bal; i++) {
+        coolmansContract.methods.tokenOfOwnerByIndex(props.userAddress, i).call().then((tokenNum) => {
+          coolmansContract.methods.tokenURI(tokenNum).call().then((ipfs) => {
+            fetch('https://ipfs.io/ipfs/' + ipfs.replace('ipfs://', ''))
+              .then((res) => {
+                return res.json()
+              }).then((obj) => {
+                let newObj = obj;
+                newObj.image = 'https://ipfs.io/ipfs/' + obj.image.replace('ipfs://', '')
+                setNfts(nfts => [...nfts, newObj])
+              })
+          })
+        })
+      }
+    })
+  }
+
+  async function loadFishyFam() {
+    // loads FishyFam
+    const fishyfamContractAddress = "0x63FA29Fec10C997851CCd2466Dad20E51B17C8aF";
+    const fishyfamContract = new window.web3.eth.Contract(fishyfamABI.abi, fishyfamContractAddress);
+    fishyfamContract.methods.walletOfOwner(props.userAddress).call().then((arrOfTokens) => {
+      for(let i = 0; i < arrOfTokens.length; i++) {
+        fishyfamContract.methods.tokenURI(arrOfTokens[i]).call().then((ipfs) => {
+          fetch(ipfs)
+            .then((res) => {
+              return res.json();
+            }).then((obj) => {
+              setNfts(nfts => [...nfts, obj])
+            })
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadCoolmans();
+    loadFishyFam();
+  }, []);
+  
+
+  return (
+    <div className='userNfts'>
+      {
+        nfts.map(nft => {
+          return (
+            <Card className='card' key={nft.name}>
+              <img src={nft.image} />
+            </Card>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default NftCards;
---
+++ src/components/wallets.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import metamask from '../assets/metamask.svg';
+import coinbasewallet from '../assets/coinbasewallet.png';
+
+const Wallets = (props) => {
+
+
+  return (
+    <Modal show={props.showWallets} onHide={()=>{props.setShowWallets(false)}}>
+      <Modal.Header closeButton>
+          <Modal.Title>Connect your wallet</Modal.Title>
+      </Modal.Header>
+      <div className="wallets">
+        <Button onClick={props.loadWeb3}>
+          <img src={metamask} alt="metamask wallet"></img>
+          <span>MetaMask</span>
+        </Button>
+        <Button onClick={props.loadWeb3}>
+          <img src={coinbasewallet} alt="coinbase wallet"></img>
+          <span>Coinbase Wallet</span>
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
+export default Wallets;
