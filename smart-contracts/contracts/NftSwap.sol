f02ebb01c440fb6de7bd7e1924eba6cb4fad8d67
alex-windows-pc <alexdziarn@gmail.com>
2022-03-14 20:58:01 -0400

    added some functions

--- smart-contracts/contracts/NftSwap.sol
+++ smart-contracts/contracts/NftSwap.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.12;
 
 import './Delegated.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract NftSwap {
 
@@ -9,31 +10,47 @@ contract NftSwap {
     string name;
     address tokenAddress;
     uint price;
+    ERC721 nftContract;
   }
 
-  uint public ownerFee;
-  uint public communityFee;
-  mapping (address => uint) public balance; //uses user address
-  mapping (address => collection) public collections; //uses contract address
+  bool public stakingLive = false;
+  uint public ownerFee = 100; // Basis Points;
+  uint public communityFee = 100;
+  mapping (address => uint) internal balance; //uses user address
+  mapping (address => collection) internal collections; //uses contract address
 
   constructor() {
-    ownerFee = 100; // Basis Points
-    communityFee = 100;
+  }
+
+  function getBalance(address userAddress) public view returns (uint) {
+    return balance[userAddress];
+  }
+
+  function getCollection(address nftAddress) public view returns (collection) {
+    return collections[nftAddress];
   }
 
   function addCollection(string name, address tokenAddress, uint price) public onlyDelegates {
-    collection newCollection = collection(name, tokenAddress, price);
+    collection newCollection = collection(name, tokenAddress, price, ERC721(tokenAddress));
     collections[tokenAddress] = newCollection;
   }
 
   function setPrices(collection[] newPrices) public onlyDelegates {
-    for(uint i=0; i < newPrices.length; i++) {
+    uint len = newPrices.length;
+    for(uint i=0; i < len; i++) {
       collections[newPrices[i].tokenAddress].price = newPrices[i].price;
     }
   }
 
-  function depositNft() public {
-    
+  function depositNft(address tokenAddress, uint[] tokenIds) public {
+    //require the person to own tokens
+    uint len = tokenIds.length;
+    collection col = collections[tokenAddress];
+    for(uint i=0; i < len; i++) {
+      balance[msg.sender] += col.price;
+      col.nftContract.transferFrom(msg.sender, address(this), tokenId);
+      // transfer ERC20 credit to msg.sender
+    }
   }
 
   function stake() public {
@@ -46,6 +63,10 @@ contract NftSwap {
 
   function unStake() public {
 
+  }
+
+  function setStaking(bool stake) public onlyDelegates {
+    stakingLive = stake;
   }
 
   function setOwnerFee(uint newFee) public onlyDelegates {
