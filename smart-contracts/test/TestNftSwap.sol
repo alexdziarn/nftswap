47fb8e5a1b7b6c7381d29d0a8ee2d1f013f58f89
alex-windows-pc <alexdziarn@gmail.com>
2022-03-18 21:32:23 -0400

    test--WIP

---
+++ smart-contracts/test/TestNftSwap.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.12;
+
+import "../contracts/NftSwap.sol";
+
+contract TestNftSwap {
+
+  
+}
--- smart-contracts/test/nftswap.js
+++ smart-contracts/test/nftswap.js
@@ -1,10 +1,12 @@
 const NftSwap = artifacts.require("NftSwap");
+const Web3 = require('web3');
 
 contract('NftSwap', (accounts) => {
-  it('should put 10000 MetaCoin in the first account', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const balance = await metaCoinInstance.getBalance.call(accounts[0]);
-
-    assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+  it('should add nft and set price to 1 NFTC', async () => {
+    const nftSwapInstance = await NftSwap.deployed();
+    // adds nft address of 0x0000000000000000000000000000000000000000 with price of 1NFTC
+    await nftSwapInstance.addNft.call("0x0000000000000000000000000000000000000000", Web3.utils.toWei('1', 'ether'));
+    const price = await nftSwapInstance.getPrice("0x0000000000000000000000000000000000000000");
+    assert.equal(price, 1, "price of nft was not 1NFTC");
   });
 });
