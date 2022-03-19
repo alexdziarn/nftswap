d1f34862b429dc81cb37140744bebcfddc8f51ea
alex-windows-pc <alexdziarn@gmail.com>
2022-03-18 21:31:14 -0400

    deployer func

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+// const Delegated = artifacts.require("Delegated");
+const NftSwap = artifacts.require("NftSwap");
+const NftSwapCredit = artifacts.require("NNftSwapCredit");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(NftSwapCredit).then(() => {
+    // deployer.deploy(Delegated);
+    deployer.deploy(NftSwap, NftSwapCredit.address);
+  });
+
 };
